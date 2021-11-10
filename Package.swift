d48f0f21295ca32e87a5fadbe469a6769f47fb0b
Marc Blanchet <marc.blanchet@viagenie.ca>
2021-11-10 16:34:24 -0500

    readding Package.swift so upstream use with Package Manager works

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "CouchbaseLiteSwift",
+    platforms: [
+        .iOS(.v9), .macOS(.v10_11)
+    ],
+    products: [
+        .library(
+            name: "CouchbaseLiteSwift",
+            targets: ["CouchbaseLiteSwift"])
+    ],
+    targets: [
+        .target(
+            name: "CouchbaseLiteSwift",
+	    dependencies: [],
+        )
+    ]
+)
