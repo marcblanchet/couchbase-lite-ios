fcb97067e7522a26cb9c399d79883ba61e223efe
Marc Blanchet <marc.blanchet@viagenie.ca>
2021-11-10 16:49:57 -0500

    add path as this repo does not have the standard Sources/$PackageName  structure

--- Package.swift
+++ Package.swift
@@ -14,7 +14,8 @@ let package = Package(
     targets: [
         .target(
             name: "CouchbaseLiteSwift",
-            dependencies: []
+            dependencies: [],
+            path: "Swift"
         )
     ]
 )
